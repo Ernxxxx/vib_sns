bf9479a6606abfbab93662120012e8eb0bc90f77
Mishimaxx <longsandao@gmail.com>
2025-12-05 14:52:38 +0900

    Webアプリ起動

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -59,4 +56,15 @@ class DefaultFirebaseOptions {
     projectId: 'vib-sns-prod',
     storageBucket: 'vib-sns-prod.firebasestorage.app',
   );
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyDVCjnjYL8FcxywHc4jwMIMDk1ZlZxExu0',
+    appId: '1:115691400203:web:9fdd9f9de392913a6eab0e',
+    messagingSenderId: '115691400203',
+    projectId: 'vib-sns-prod',
+    authDomain: 'vib-sns-prod.firebaseapp.com',
+    storageBucket: 'vib-sns-prod.firebasestorage.app',
+    measurementId: 'G-GBYDE6M5YM',
+  );
+
 }
